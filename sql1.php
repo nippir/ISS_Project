593c0bb4abe721497ab1b3f387307c5805b32c40
nippir <59783232+nippir@users.noreply.github.com>
2023-01-21 11:46:05 +0530

    Update sql1.php

--- sql1.php
+++ sql1.php
@@ -21,13 +21,7 @@
 
 
 <?php 
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$db = "1ccb8097d0e9ce9f154608be60224c7c";
-
-	// Create connection
-	$conn = mysqli_connect($servername,$username,$password,$db);
+	include conn.php:
 
 	// Check connection
 	if (!$conn) {
