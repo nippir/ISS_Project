22347842d0fa7d5953ac0d02a446ce764f329e0a
nippir <59783232+nippir@users.noreply.github.com>
2023-01-21 11:45:15 +0530

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$db = "1ccb8097d0e9ce9f154608be60224c7c";
+
+	// Create connection
+	$conn = mysqli_connect($servername,$username,$password,$db);
+?>
